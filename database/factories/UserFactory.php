f61e2ea5326ad31b2864e8f3b942352322efbb53
Shift <shift@laravelshift.com>
2024-04-02 01:11:33 +0000

    Add type hints for Laravel 10

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,7 @@ class UserFactory extends Factory
      */
     protected static ?string $password;
 
-    public function definition()
+    public function definition(): array
     {
         return [
             'first_name' => $this->faker->firstName(),
--- database/migrations/2024_04_02_000000_add_expires_at_to_personal_access_tokens_table.php
+++ database/migrations/2024_04_02_000000_add_expires_at_to_personal_access_tokens_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
             $table->timestamp('expires_at')->nullable()->after('last_used_at');
@@ -22,7 +22,7 @@ return new class extends Migration {
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::table('personal_access_tokens', function (Blueprint $table) {
             $table->dropColumn('expires_at');
