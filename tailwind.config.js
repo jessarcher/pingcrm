07a822cd20a9a65328a634ff003cfa0dace09326
Shift <shift@laravelshift.com>
2024-04-02 01:30:15 +0000

    Convert to Tailwind 3.0 colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+let colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./resources/**/*.{js,vue,blade.php}'],
+  theme: {
+    extend: {
+      colors: {
+        yellow: colors.amber,
+        green: colors.emerald,
+      },
+    },
+  },
 }
